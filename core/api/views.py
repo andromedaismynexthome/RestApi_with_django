7b72f4707e217820e5e73fdf2c092c7386e45318
Dev <downlaodsnowinx2speed.com>
2024-01-15 14:22:59 +0530

    CompanyViewset created

--- core/api/views.py
+++ core/api/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from api.models import Company
+from api.serializers import Companyserializer
+
 
 # Create your views here.
+class Companyviewset(viewsets.ModelViewSet):
+    queryset = Company.objects.all()
+    serializer_class = Companyserializer
+    
