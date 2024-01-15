a789dd8edcf76023460f4b43106e09cfddae2854
Dev <downlaodsnowinx2speed.com>
2024-01-15 14:31:54 +0530

    done with creating company api

---
+++ core/api/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path,include
+from api.views import Companyviewset
+from rest_framework import routers
+router = routers.DefaultRouter()
+router.register(r'companies',Companyviewset)
+urlpatterns = [
+  path('',include(router.urls))
+]
