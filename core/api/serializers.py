be5edc569d4cce2f53ce409e5feca7c1a7312b6e
Dev <downlaodsnowinx2speed.com>
2024-01-16 12:04:21 +0530

    done with creating employee serializer

--- core/api/serializers.py
+++ core/api/serializers.py
@@ -1,9 +1,16 @@
 from rest_framework import serializers
-from api.models import Company
+from api.models import Company, Employee
+
+
 # creating comapny serializers
 class Companyserializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Company
         fields = "__all__"
         
-        
+#creating employeeserializer
+class Employeeserializer(serializers.HyperlinkedModelSerializer):
+    Employee_id = serializers.ReadOnlyField()
+    class Meta:
+        model = Employee
+        fields = "__all__"
