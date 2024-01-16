e479fc16197b088ee66c24f01768c5a9820043ef
Dev <downlaodsnowinx2speed.com>
2024-01-16 12:45:06 +0530

    we created custom url

--- core/api/views.py
+++ core/api/views.py
@@ -2,12 +2,22 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from api.models import Company, Employee
 from api.serializers import Companyserializer, Employeeserializer
-
-
+from rest_framework.decorators import action
+from rest_framework.response import Response
 # Create your views here.
 class Companyviewset(viewsets.ModelViewSet):
     queryset = Company.objects.all()
     serializer_class = Companyserializer
+
+    # we creating custom urls companies/primarykey/employees
+    @action(detail=True, methods=['get'])
+    def employees(self,request,pk=None):
+        company = Company.objects.get(pk=pk)
+        emps = Employee.objects.filter(company=company)
+        emps_serializer = Employeeserializer(emps,many=True,context={'request':request})
+        return Response(emps_serializer.data)
+
+
 
 #creating employee viewset
 class Employeeviewset(viewsets.ModelViewSet):
