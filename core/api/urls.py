0c581dd8093028183023f880d41b1034c4b67c30
Dev <downlaodsnowinx2speed.com>
2024-01-16 12:17:35 +0530

    done creating employee routers

--- core/api/urls.py
+++ core/api/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path,include
-from api.views import Companyviewset
+from api.views import Companyviewset, Employeeviewset
 from rest_framework import routers
 router = routers.DefaultRouter()
 router.register(r'companies', Companyviewset)
+#employee routes
+router.register(r'employees', Employeeviewset)
 
 urlpatterns = [
   path('',include(router.urls))
