4f24fd0331f656378e18110f5cd8547ab033d0b2
Dev <downlaodsnowinx2speed.com>
2024-01-16 12:12:31 +0530

    done with Employee Viewset

--- core/api/views.py
+++ core/api/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from rest_framework import viewsets
-from api.models import Company
-from api.serializers import Companyserializer
+from api.models import Company, Employee
+from api.serializers import Companyserializer, Employeeserializer
 
 
 # Create your views here.
@@ -9,3 +9,7 @@ class Companyviewset(viewsets.ModelViewSet):
     queryset = Company.objects.all()
     serializer_class = Companyserializer
 
+#creating employee viewset
+class Employeeviewset(viewsets.ModelViewSet):
+    queryset = Employee.objects.all()
+    serializer_class = Employeeserializer
